ff838e0f7b58c2e419fcac39093ab5f7614f2a08
basilabuisleem <basil.abuisleem@gmail.com>
2023-01-02 22:55:29 +0300

    updateAdminside

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,67 @@
+  <!-- App Settings FAB -->
+  <div id="app-settings">
+    <app-settings layout-active="default"
+                  :layout-location="{
+'default': 'index.html',
+'fixed': 'fixed-dashboard.html',
+'fluid': 'fluid-dashboard.html',
+'mini': 'mini-dashboard.html'
+}"></app-settings>
+</div>
+
+<!-- jQuery -->
+<script src="assets/vendor/jquery.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/vendor/popper.min.js"></script>
+<script src="assets/vendor/bootstrap.min.js"></script>
+
+<!-- Perfect Scrollbar -->
+<script src="assets/vendor/perfect-scrollbar.min.js"></script>
+
+<!-- DOM Factory -->
+<script src="assets/vendor/dom-factory.js"></script>
+
+<!-- MDK -->
+<script src="assets/vendor/material-design-kit.js"></script>
+
+<!-- App -->
+<script src="assets/js/toggle-check-all.js"></script>
+<script src="assets/js/check-selected-row.js"></script>
+<script src="assets/js/dropdown.js"></script>
+<script src="assets/js/sidebar-mini.js"></script>
+<script src="assets/js/app.js"></script>
+
+<!-- App Settings (safe to remove) -->
+<script src="assets/js/app-settings.js"></script>
+
+<!-- Flatpickr -->
+<script src="assets/vendor/flatpickr/flatpickr.min.js"></script>
+<script src="assets/js/flatpickr.js"></script>
+
+<!-- Global Settings -->
+<script src="assets/js/settings.js"></script>
+
+<!-- Moment.js -->
+<script src="assets/vendor/moment.min.js"></script>
+<script src="assets/vendor/moment-range.js"></script>
+
+<!-- Chart.js -->
+<script src="assets/vendor/Chart.min.js"></script>
+
+<!-- App Charts JS -->
+<script src="assets/js/charts.js"></script>
+<script src="assets/js/chartjs-rounded-bar.js"></script>
+
+<!-- Chart Samples -->
+<script src="assets/js/page.dashboard.js"></script>
+<script src="assets/js/progress-charts.js"></script>
+
+<!-- Vector Maps -->
+<script src="assets/vendor/jqvmap/jquery.vmap.min.js"></script>
+<script src="assets/vendor/jqvmap/maps/jquery.vmap.world.js"></script>
+<script src="assets/js/vector-maps.js"></script>
+
+</body>
+
+</html>
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,22 +1,32 @@
-@extends('layout.admin.index' )
-@section('title')
-   Edit Products
-@stop
-@section('extra_css')
-   <!-- the script in this page wont work with pjax so i hava to reload it  -->
-   @if (env('APP_AJAX'))
-      <script type="text/javascript">
-          document.on('pjax:complete', function () {
-              pjax.reload();
-          })
-      </script>
-   @endif
-@stop
-
+@extends('layout.admin.app')
 @section('content')
 
-   <div class="row" style="">
-      <form method="post" action="{{ route('product.update',$product->product_id) }}" enctype="multipart/form-data"
+
+
+<div class="container-fluid page__heading-container">
+    <div class="page__heading">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="#"><i class="material-icons icon-20pt">home</i></a></li>
+                <li class="breadcrumb-item">UI Components</li>
+                <li class="breadcrumb-item active"
+                    aria-current="page">Edit Products</li>
+            </ol>
+        </nav>
+
+        <h1 class="m-0">Edit Products</h1>
+    </div>
+</div>
+
+<div class="container-fluid page__container">
+
+
+
+    <div class="card card-form">
+        <div class="row no-gutters" style="padding: 2%">
+         {{-- ========================edit Products=============================== --}}
+
+         <form method="post" action="{{ route('product.update',$product->product_id) }}" enctype="multipart/form-data"
             id="Uproduct_form">
          @csrf
          @if( ! env("APP_AJAX") )
@@ -239,300 +249,27 @@
             </div>
          </div>
       </form>
-   </div>
+         {{-- ======================== End edit Products=============================== --}}
+        
+        </div>
+    </div>
 
-@endsection()
-@section('extra_js')
-   <script type="text/javascript">
-       // show items
-       function showMe() {
-           jQuery(".available0").toggle();
-       }
 
-       function showDiscount() {
-           jQuery(".div-discount").toggle();
-       }
 
-       // <!-- add site map of the page -->
-       jQuery(document).one('load', function (e) {
-           jQuery("#site_map").append("<i class='ace-icon fa '></i><a href='{{ route('product.create') }}' class='click_me'>Create Product</a>");
-           // e.isImmediatePropagationStopped()
-       });
-   </script>
-   <!-- inline scripts related to this page -->
-   <script type="text/javascript">
-       jQuery(document).ready(function ($) {
-           var demo1 = jQuery('select[name="categories[]"]').bootstrapDualListbox({infoTextFiltered: '<span class="label label-purple label-lg">Filtered</span>'});
-           var container1 = demo1.bootstrapDualListbox('getContainer');
-           container1.find('.btn').addClass('btn-white btn-info btn-bold');
-           //typeahead.js
-           //example taken from plugin's page at: https://twitter.github.io/typeahead.js/examples/
-           var substringMatcher = function (strs) {
-               return function findMatches(q, cb) {
-                   var matches, substringRegex;
-                   // an array that will be populated with substring matches
-                   matches = [];
-                   // regex used to determine if a string contains the substring `q`
-                   substrRegex = new RegExp(q, 'i');
-                   // iterate through the pool of strings and for any string that
-                   // contains the substring `q`, add it to the `matches` array
-                   jQuery.each(strs, function (i, str) {
-                       if (substrRegex.test(str)) {
-                           // the typeahead jQuery plugin expects suggestions to a
-                           // JavaScript object, refer to typeahead docs for more info
-                           matches.push({value: str});
-                       }
-                   });
-                   cb(matches);
-               }
-           }
-           jQuery('input.typeahead').typeahead({
-               hint: true,
-               highlight: true,
-               minLength: 1
-           }, {
-               name: 'states',
-               displayKey: 'value',
-               source: substringMatcher(ace.vars['US_STATES']),
-               limit: 10
-           });
-           //in ajax mode, remove remaining elements before leaving page
-           jQuery(document).one('ajaxloadstart.page', function (e) {
-               jQuery('[class*=select2]').remove();
-               jQuery('select[name="categories[]"]').bootstrapDualListbox('destroy');
-               jQuery('.rating').raty('destroy');
-               jQuery('.multiselect').multiselect('destroy');
-           });
+    <!-- <div class="card card-form">
+<div class="row no-gutters">
+<div class="col-lg-4 card-body">
+<p><strong class="headings-color">Basic Information</strong></p>
+<p class="text-muted">Edit your account details and settings.</p>
+</div>
+<div class="col-lg-8 card-form__body card-body">
 
-           var demo1 = jQuery('select[name="colors[]"]').bootstrapDualListbox({infoTextFiltered: '<span class="label label-purple label-lg">Filtered</span>'});
-           var container1 = demo1.bootstrapDualListbox('getContainer');
-           container1.find('.btn').addClass('btn-white btn-info btn-bold');
-           //typeahead.js
-           //example taken from plugin's page at: https://twitter.github.io/typeahead.js/examples/
-           var substringMatcher = function (strs) {
-               return function findMatches(q, cb) {
-                   var matches, substringRegex;
-                   // an array that will be populated with substring matches
-                   matches = [];
-                   // regex used to determine if a string contains the substring `q`
-                   substrRegex = new RegExp(q, 'i');
-                   // iterate through the pool of strings and for any string that
-                   // contains the substring `q`, add it to the `matches` array
-                   jQuery.each(strs, function (i, str) {
-                       if (substrRegex.test(str)) {
-                           // the typeahead jQuery plugin expects suggestions to a
-                           // JavaScript object, refer to typeahead docs for more info
-                           matches.push({value: str});
-                       }
-                   });
-                   cb(matches);
-               }
-           }
-           jQuery('input.typeahead').typeahead({
-               hint: true,
-               highlight: true,
-               minLength: 1
-           }, {
-               name: 'states',
-               displayKey: 'value',
-               source: substringMatcher(ace.vars['US_STATES']),
-               limit: 10
-           });
-           //in ajax mode, remove remaining elements before leaving page
-           jQuery(document).one('ajaxloadstart.page', function (e) {
-               jQuery('[class*=select2]').remove();
-               jQuery('select[name="categories[]"]').bootstrapDualListbox('destroy');
-               jQuery('.rating').raty('destroy');
-               jQuery('.multiselect').multiselect('destroy');
-           });
-       });
-   </script>
-   {{--send date with AJAX--}}
-   @if(env('APP_AJAX') )
-      <script type="text/javascript">
-          $(document).ready(function () {
-              //get value of radio buttons
-              var radio_val = $("input[name='cover']").val();
-              $(".radio").click(function () {
-                  radio_val = $(this).val();
-              });
-              $("#Uproduct_form").submit(function (e) {
-                  e.preventDefault();
-                  var form = $(this);
-                  var data_form = new FormData(this);
-                  data_form.append('_method', 'PUT');
-                  /*var data = {
-                      product_name: $("#product_name").val(),
-                      made_in: $("#made_in").val(),
-                      brand_id: $("#brand_id").val(),
-                      product_slug: $("#product_slug").val(),
-                      sale_price: $("#sale_price").val(),
-                      buy_price: $("#buy_price").val(),
-                      quantity: $("#quantity").val(),
-                      weight: $("#weight").val(),
-                      description: $("#description").val(),
-                      data_available: $("#data_available").val(),
-                      off_price: $("#off_price").val(),
-                      categories: $("#duallist0").val(),
-                      colors: $("#duallist").val(),
-                      photos: $("#gallery-photo-add").val(),
-                      cover: radio_val,
-                  };*/
-                  // check if the input is valid
-                  // if (!form.valid()) return false;
-                  $.ajaxSetup({
-                      headers: {
-                          'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
-                      }
-                  });
-                  $.ajax({
-                      url: "{{ route('product.update',$product->product_id) }}",
-                      method: "POST",
-                      enctype: 'multipart/form-data',
-                      dataType: 'json',
-                      data: data_form,
-                      contentType: false,
-                      cache: false,
-                      processData: false,
-                      beforeSend: function () {
-                          $(".preview").toggle();
-                      },
-                      success: function (data) {
-                          console.log(data.message);
-                          if (data.success === true) {
-                              alert(data.message);
-                              $("#error_result").empty();
-                          }
-                          //show loading image ,
-                          $(".preview").hide();
-                      },
-                      error: function (request, status, error) {
-                          console.log(request.responseText);
-                          var json = $.parseJSON(request.responseText);
-                          if (json.success === false) {
-                              alert(json.message);
-                              $(".preview").hide();
-                              return
-                          }
-                          $("#error_result").empty();
-                          $(".preview").hide();
-                          $.each(json.errors, function (key, value) {
-                              $('.alert-danger').show().append('<p>' + value + '</p>');
-                          });
-                          $('html, body').animate(
-                              {
-                                  scrollTop: $("#error_result").offset().top,
-                              },
-                              500,
-                          )
-                          // $("#result").html('');
-                      }
-                  });
-              });
-          });
-      </script>
-   @endif
+</div>
+</div>
+</div> -->
+</div>
 
-   <script type="text/javascript">
-       <!-- show selected images -->
-       $(document).ready(function () {
-           $(function () {
-               // Multiple images preview in browser
-               var imagesPreview = function (input, placeToInsertImagePreview) {
-                   // $(".gallery").empty();
-                   if (input.files) {
-                       var filesAmount = input.files.length;
-                       $(".ace-file-name").attr('data-title', 'now choose the cover');
-                       var c = 0;
-                       for (var i = 0; i < filesAmount; ++i) {
-                           var reader = new FileReader();
+</div>
 
-                           reader.onload = function (event) {
-                               var showImage = '<div class="fileuploader-items div-show">' +
-                                   '<ul class="fileuploader-items-list">' +
-                                   '<li class="fileuploader-item file-has-popup file-type-image file-ext-png">' +
-                                   '<div class="columns"><div class="column-thumbnail"><div class="fileuploader-item-image fileuploader-no-thumbnail">' +
-                                   '<div style="background-color: #298a22" class="fileuploader-item-icon">' +
-                                   '<img src="' + event.target.result + '" alt="' + input.files[c].type + '"></div></div><span class="fileuploader-action-popup"></span></div>' +
-                                   '<div class="column-title">' +
-                                   '<div title="innostudio.de__setting-icnload.png">' + input.files[c].name + '</div>' +
-                                   '<span>' + (input.files[c].size) + ' KB </span></div>' +
-                                   '<div class="column-actions">' +
-                                   '<input type="radio" name="cover" value="' + input.files[c].name + '" class="cover_cb fileuploader-action radio" id="cover" title="Cover Photo"><i class="icon ui-icon-asc"></i>' +
-                                   '<a class="fileuploader-action fileuploader-action-remove" title="remove">' +
-                                   '<i onclick="removeImage(this)"></i></a>' +
-                                   '</div></div><div class="progress-bar2"><div class="fileuploader-progressbar"><div class="bar"></div></div><span></span>' +
-                                   '</div></li></ul></div>\n';
-                               $(".gallery").append(showImage);
-                               c++;
-                           }
-                           reader.readAsDataURL(input.files[i]);
-                       }
-                   }
-               };
 
-               $('#gallery-photo-add').on('change', function () {
-                   imagesPreview(this, 'div.gallery');
-               });
-           });
-       });
-
-       //REMOVE IMAGES WHICH SELECTED FRIM INPUT
-       function removeImage(e) {
-           $(e).parents(":eq(4)").remove()
-       }
-
-       //REMOVE IMAGES FROM DATA BASE with ajax
-       $(".destroy_image").click(function () {
-           var obj = $(this);
-           var photo_id = $(this).data("id");
-           $.ajaxSetup({
-               headers: {
-                   'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-               }
-           });
-           $.ajax({
-               url: " /admin/photo/" + photo_id,
-               method: "DELETE",
-               datatype: "json",
-               data: {id: photo_id},
-               success: function (data) {
-                   if ( data.success === true) {
-
-                       alert(data.message);
-                       obj.parents(":eq(5)").remove();
-                   }
-               },
-               error: function (request, status, error) {
-                   console.log(request);
-                   var json = $.parseJSON(request.responseText);
-                   if (json.success === false) {
-                       alert(json.message);
-                   }
-                   console.log(json,error);
-               }
-           });
-       });
-   </script>
-   <script src="{{ asset('admin-assets/js/bootstrap-tag.min.js') }}"></script>
-   <!-- FOR TAG INPUT -->
-   <script type="text/javascript">
-       var tag_input = $('#form-field-tags');
-       try {
-           tag_input.tag(
-               {
-                   source: function (query, process) {
-                       $.ajax({
-                           url: '/admin/product/tags/' + query,
-                           type: 'get'
-                       }).done(function (result_items) {
-                           process(result_items);
-                       });
-                   }
-               }
-           )
-       } catch (e) {
-           tag_input.after('<textarea id="' + tag_input.attr('id') + '" name="' + tag_input.attr('name') + '" rows="3">' + tag_input.val() + '</textarea>').remove();
-       }
-   </script>
-@stop
+@endsection   
