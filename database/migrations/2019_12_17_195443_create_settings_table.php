597cb6bc9bbf07d4aa27b58ea05c79f926b11082
rakad-dwairi <rakadissam@gmail.com>
2023-01-03 20:49:04 +0200

    Migration Update On Settings

--- database/migrations/2019_12_17_195443_create_settings_table.php
+++ database/migrations/2019_12_17_195443_create_settings_table.php
@@ -23,9 +23,9 @@ class CreateSettingsTable extends Migration
             $table->string('site_phone');
             $table->string('site_email');
             $table->string('site_fax');
-            $table->string('slider_image1');
-            $table->string('slider_image2');
-            $table->string('slider_image3');
+            $table->string('slider_image1')->nullable();
+            $table->string('slider_image2')->nullable();
+            $table->string('slider_image3')->nullable();
         });
     }
 
